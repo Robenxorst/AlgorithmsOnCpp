cc191689b05699ddc4cbe808d8244765fcacb5f4
NIKOLAY POLOVNIKOV <82658563+Robenxorst@users.noreply.github.com>
2022-03-04 00:59:53 +0300

    Update Airplane.cpp

--- Airplane.cpp
+++ Airplane.cpp
@@ -24,6 +24,52 @@
 // где занятые группой место обозначены x.
 // Если нет мест : Cannot fulfill passengers requirements
 
+//Тест
+
+//Ввод
+//4
+//..._.#.
+//.##_...
+//.#._.##
+//..._...
+//7
+//2 left aisle
+//3 right window
+//2 left window
+//3 left aisle
+//1 right window
+//2 right window
+//1 right window
+
+// Вывод
+//Passengers can take seats: 1B 1C
+//.XX_.#.
+//.##_...
+//.#._.##
+//..._...
+//        Passengers can take seats: 2D 2E 2F
+//.##_.#.
+//.##_XXX
+//.#._.##
+//..._...
+//        Passengers can take seats: 4A 4B
+//.##_.#.
+//.##_###
+//.#._.##
+//XX._...
+//        Cannot fulfill passengers requirements
+//        Passengers can take seats: 1F
+//.##_.#X
+//.##_###
+//.#._.##
+//##._...
+//        Passengers can take seats: 4E 4F
+//.##_.##
+//.##_###
+//.#._.##
+//##._.XX
+//        Cannot fulfill passengers requirements
+
 #include <vector>
 #include <string>
 #include <iostream>
